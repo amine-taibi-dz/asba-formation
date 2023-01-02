9250bc978fd652e0b1400b34d28144b117ab6765
Amine TAIBI <48686078+amine-taibi-dz@users.noreply.github.com>
2023-01-02 08:29:33 +0100

    Sample 11
    Example CDI enbaled

---
+++ web-cdi-ws/src/main/java/dz/acs/fact/biz/FacturationEjb.java
@@ -0,0 +1,20 @@
+package dz.acs.fact.biz;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class FacturationEjb
+ */
+@Stateless
+@LocalBean
+public class FacturationEjb {
+
+    /**
+     * Default constructor. 
+     */
+    public FacturationEjb() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
